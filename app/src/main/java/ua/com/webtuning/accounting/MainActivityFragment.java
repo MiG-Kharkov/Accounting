0712db01865778c047f72d042be16e901aa565f3
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-10 15:27:36 +0300

    Add list + adapter with tast data

--- app/src/main/java/ua/com/webtuning/accounting/MainActivityFragment.java
+++ app/src/main/java/ua/com/webtuning/accounting/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package ua.com.webtuning.accounting;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> mAccountingAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,35 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Create some temp data for ListView
+        String [] data= {
+                "Expense 1 - 100 grn",
+                "Expense 2 - 200 grn",
+                "Expense 3 - 300 grn",
+                "Expense 4 - 400 grn",
+                "Expense 5 - 500 grn",
+                "Expense 6 - 600 grn",
+                "Expense 7 - 700 grn",
+                "Expense 8 - 800 grn",
+                "Expense 9 - 900 grn",
+                "Expense 10 - 110 grn",
+                "Expense 11 - 120 grn",
+                "Expense 12 - 130 grn",
+        };
+
+        List<String> listExpenses = new ArrayList<String>(Arrays.asList(data));
+        mAccountingAdapter = new ArrayAdapter<String>(
+                getActivity(), //this activity
+                R.layout.list_item_expense, //Name of the layout
+                R.id.list_item_expense_textview,  // The ID of the textview
+                listExpenses); //data
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_expense);
+        listView.setAdapter(mAccountingAdapter);
+
+        return rootView;
     }
 }
